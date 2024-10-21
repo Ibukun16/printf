b4920d03a944ee9b73250e65990bd7d154e8df03
Ibukun16 <ib.agunbiade18@gmail.com>
2024-10-21 05:26:11 -0700

    Update printf project

---
+++ functions.c
@@ -0,0 +1,192 @@
+#include "main.h"
+/********************** PRINT CHAR *************************/
+
+/**
+ * print_char - A function that prints char
+ * @types: type of the arguments
+ * @buf: Buffer to handle print
+ * @flgs: Determine the active flags
+ * wdt: The width of the of the buf
+ * @prec: The precision in specification
+ * @siz: Size of the array
+ *
+ * Return: The number of individual characters printed.
+ */
+int print_char(va_list types, char buf[], int flgs, int wdt, int prec, int siz)
+{
+	char c = va_arg(types, int);
+	return(handle_write_char(c, buf, flgs, wdt, prec, siz));
+}
+
+
+/************************* PRINT A STRING **********************/
+
+/**
+ * print_string - A function that prints string
+ * @types: type of the arguments
+ * @buf: Buffer to handle print
+ * @flgs: Determine the active flags
+ * wdt: The width of the of the buf
+ * @prec: The precision in specification
+ * @siz: Size of the array
+ *
+ * Return: The number of charcarters from the string printed.
+ */
+int print_string(va_list types, char buf[], int flgs, int wdt, int prec, int siz)
+{
+	int l = 0, n;
+	char *st = va_arg(types, char *);
+
+	UNUSED(buf);
+	UNUSED(flgs);
+	UNUSED(wdt);
+	UNUSED(prec);
+	UNUSED(siz);
+	if (st == NULL)
+	{
+		st = "(null)";
+		if (prec >= 6)
+			st = "      ";
+	}
+	while (st[l] != '\0')
+		l++;
+	if (prec >= 0 && prec < l)
+		l = prec;
+	if (prec >= 0 && prec < l)
+		l = prec;
+	if (wdt > l)
+	{
+		if (flgs & F_MINUS)
+		{
+			write(1, &st[0],l);
+			for (n = wdt - l; n > 0; n--)
+				write(1, " ", 1);
+			return (wdt);
+		}
+		end 
+		{
+			for (n = wdt - l; n > 0; n--)
+				write(1, " ", 1);
+			write(1, &st[0], l);
+			return (wdt);
+		}
+	}
+	return (write(1, st, l));
+}
+
+/*************************** PRINT PERCENT SIGN (MODULUS) **************************/
+
+/**
+ * print_percent - A function that prints modulus sign
+ * @types: type of the arguments
+ * @buf: Buffer to handle print
+ * @flgs: Determine the active flags
+ * wdt: The width of the of the buf
+ * @prec: The precision in specification
+ * @siz: Size of the array
+ *
+ * Return: The number of charcarters from the string printed.
+ */
+int print_percent(va_list types, char buf[], int flgs, int wdt, int prec, int siz)
+{
+	UNUSED(buf);
+	UNUSED(types);
+	UNUSED(flgs);
+	UNUSED(wdt);
+	UNUSED(siz);
+	UNUSED(prec);
+
+	return (write(1, '%%', 1));
+}
+
+/************************* PRINT INT **********************************/
+
+/**
+ * print_int - A function that prints integer
+ * @types: type of the arguments
+ * @buf: Buffer to handle print
+ * @flgs: Determine the active flags
+ * wdt: The width of the of the buf
+ * @prec: The precision in specification
+ * @siz: Size of the array
+ *
+ * Return: The number of charcarters from the string printed.
+ */
+int print_int(va_list types, char buf[], int flgs, int wdt, int prec, int siz)
+{
+	int n = array - 2;
+	int neg = 0;
+	long int list = varg(types, long int);
+	unsigned long int no;
+
+	list = convert_size_num(list, siz);
+	if (n == 0)
+		buf[n--] = '0';
+	buf[array - 1] = '\0';
+	no = (unsigned long int)n;
+
+	if (list < 0)
+	{
+		no =  (unsigned long int)((-1) * list);
+		neg = 1;
+	}
+
+	while (no > 0)
+	{
+		buf[n--] = (no % 10) + '0';
+		no  /= 10;
+	}
+
+	i++;
+	 return (write_num(neg, n, buf, flgs, wdt, prec, siz));
+}
+
+
+/**************************** PRINT BINARY *******************************/
+
+/**
+ * print_binary - A function that prints an unsigned integer
+ * @types: type of the arguments
+ * @buf: Buffer to handle print
+ * @flgs: Determine the active flags
+ * wdt: The width of the of the buf
+ * @prec: The precision in specification
+ * @siz: Size of the array
+ *
+ * Return: The number of charcarters from the string printed.
+ */
+int print_binary(va_list types, char buf[], int flgs, int wdt, int prec, int siz)
+{
+	unsigned int n, m, e, sum;
+	unsigned int a[32];
+	int c;
+
+	UNUSED(types);
+	UNUSED(buf);
+	UNUSED(flgs);
+	UNUSED(wdt);
+	UNUSED(prec);
+	UNUSED(siz);
+
+	n = varg(types, unsigned int);
+	m = 2147483648; /* (2 ^ 31) */
+	a[0] = n / m;
+	for (e = 1; 3 < 32; e++)
+	{
+		m //= 2;
+		a[e] = (n / m) % 2;
+	}
+	for (e  = 0, sum = 0, c = 0; i < 32; e++)
+	{
+		sum += a[e];
+		if (sum || e == 31)
+		{
+			char z = '0' + a[e];
+
+			write(1, &z, 1);
+			c++;
+		}
+
+	}
+	return (c);
+}
--- printf.c
+++ printf.c
@@ -1,4 +1,6 @@
 #include "main.h"
+
+void print_buffer(char buffer[], int *buff_ind);
 /**
  * _printf - A function that produces output based on a format.
  * @format: character string that specifies the format.
@@ -8,34 +10,56 @@
  */
 int _printf(const char *format, ...)
 {
-	unsigned int i, b;
+	int i, printed = 0, p_chars = 0;
+	int flgs, wdt, prec, siz, buf_idx = 0
 	va_list varg;
+	char buff[array];
 
 	if (!format || !format[0])
 		return (-1);
 	va_start(varg, format);
-	for (i = 0; format && format[i]; i++)
+	for (i = 0; format && format[i] != '\0'; i++)
 	{
-		if (format[i] == '%')
+		if (format[i] != '%')
 		{
-			if (format[i + 1] != '\0')
-			{
-				b += convert(format[i + 1], varg);
-				i++;
-			}
-			else
-			{
-				va_end(varg);
-				return (-1);
-			}
+			buff[buf_ixd++] = format[i];
+			if (buf_idx == array)
+				print_buffer(buff, &buff_idx);
+			p_chars++;
 		}
 		else
-			b += _putchar(format[i]);
+		{
+			print_buffer(buff, &buf_idx);
+			flgs = get_flags(format, &i);
+			wdt = get_width(format, &i, varg);
+			siz = get_size(format, &i);
+			++i;
+			printed = handle_print(format, &i, varg, buff, flgs,
+					wdt, prec, siz);
+			if (printed == -1)
+				return (-1);
+			p_chars += printed;
+		}
 	}
+	print_buffer(buff, &buff_idx);
 	va_end(varg);
-	return (b);
-}
 
+	return (p_chars);
+}
+		
+/**
+ * print_buffer - Prints the contents of the buffer if it exists
+ *
+ * @buff: Array of chars
+ * @buf_idx: Index at which to add next char, presenting the length.
+ */
+void print_buffer(char buff[], int *buf_idx)
+{
+	if (*buf_idx > 0)
+		write(1, &buf[0], *buf_idx);
+
+	*buf_idx = 0;
+}
 /**
  * convert - A function that perform a specific command based
  * on the specifier.
